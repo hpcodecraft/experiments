0b893b6ce1d083a7251a03b0eba67b2b7576a7a2
hpcodecraft <thesquidpeople@googlemail.com>
2016-03-12 14:21:23 +0100

    Updates Leapmotion scripts, switches circles for finger position for a 3D hand model

--- gumspin/js/gumspin-leapmotion.js
+++ gumspin/js/gumspin-leapmotion.js
@@ -1,33 +1,5 @@
-// canvas setup
-var canvas = document.getElementById('canvas'),
-    c =  canvas.getContext( '2d' ),
-    width = canvas.width,
-    height = canvas.height;
-
-// helper function
-function leapToScene( frame , leapPos ){
-
-  if("undefined" === typeof leapPos) return;
-
-  var iBox = frame.interactionBox;
-
-  var top = iBox.center[1] + iBox.size[1]/2;
-  var left = iBox.center[0] - iBox.size[0]/2;
-
-  var x = leapPos[0] - left;
-  var y = leapPos[1] - top;
-
-  x /= iBox.size[0];
-  y /= iBox.size[1];
-
-  x *= width;
-  y *= height;
-
-  return [ x , -y ];
-}
-
 // leap event handlers
-var controller = new Leap.Controller({ enableGestures: true });
+var controller = new Leap.Controller({ enableGestures: true }).use('riggedHand');
 
 function setConnected() {
   document.querySelector('.leapmotion-status').setAttribute('class','leapmotion-status connected');
@@ -41,9 +13,9 @@ function setDisconnected() {
   device = 'Mouse';
 }
 
-controller.on('deviceConnected', setConnected);
+controller.on('streamingStarted', setConnected);
 controller.on('ready', setConnected);
-controller.on('deviceDisconnected', setDisconnected);
+controller.on('streamingStopped', setDisconnected);
 
 controller.on('animationFrame', function(frame) {
 
@@ -62,33 +34,6 @@ controller.on('animationFrame', function(frame) {
         }
 
         if(!animationRunning) animateKelly();
-    }
-  }
-
-  // draw fingers
-  c.clearRect(0, 0, width, height);
-
-  for( var i=0; i < frame.hands.length; i++ ){
-
-    var hand = frame.hands[i];
-
-    for( var j = 0; j < hand.fingers.length; j++ ){
-
-      var finger = hand.fingers[j];
-
-      // Getting our finger position
-      var fingerPos = leapToScene( frame , finger.tipPosition );
-      //if(!fingerPos) continue;
-
-      // Setting up the style for the stroke
-      c.fillStyle = "#01ac5e";
-      //c.lineWidth = 5;
-
-      // Draw a circle
-      c.beginPath();
-      c.arc(fingerPos[0], fingerPos[1], 8, 0, Math.PI*2);
-      c.closePath();
-      c.fill();
     }
   }
 });
